0c405d3270d4d6b108263de96b7b29dbdf27958d
SergLavrov <ls500ls@mail.ru>
2023-10-25 19:10:33 +0300

    Initial commit Release v_0.0.1 vm_get_workers

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 import psycopg2
 from dotenv import dotenv_values
+from pydantic import BaseModel
+import traceback
 
 config = dotenv_values(".env")
 
@@ -18,4 +20,45 @@ app = FastAPI()
 
 @app.get("/")
 def root():
-    return {"message": "Hello World"}
+    return {"message": "Start Server"}
+
+
+class vm_get_workers(BaseModel):
+    id: int
+    firstName: str
+    position: str
+    phone: str
+
+
+class vm_get_worker(BaseModel):
+    id: int
+    firstName: str
+    surName: str
+    email: str
+    phone: str
+    address: str
+    position: str
+    salary: float
+    departmentName: str
+
+
+@app.get("/get-workers")
+def get_workers():
+    try:
+        cursor.execute("""
+            SELECT id, firstName, position, phone
+            FROM worker;
+        """)
+        result = cursor.fetchall()
+        list_workers = []
+        for worker in result:
+            list_workers.append(vm_get_workers(
+                id=worker[0],
+                firstName=worker[1],
+                position=worker[2],
+                phone=worker[3]
+            ))
+
+        return {"workers": list_workers}
+    except:
+        return {"error": traceback.format_exc()}
